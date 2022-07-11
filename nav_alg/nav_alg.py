192ca72ae38bcf3904914cc5508d3c12c597f936
juntang <richard_tang_98@163.com>
2022-07-11 11:42:20 +0800

    2022/7/11

--- nav_alg/nav_alg.py
+++ nav_alg/nav_alg.py
@@ -1,3 +1,4 @@
+import math
 from abc import ABCMeta,abstractmethod
 from .state import JointState,FullState,ObservableState
 from .action import ActionRot,ActionXY
@@ -13,22 +14,32 @@ class NavAlg(metaclass = ABCMeta):
         self.config.read(config_path)
         self.policy = policy_factory[policy]
         self.device = None
-    
 
-    def setup(self):
-        self.device = torch.device("cuda:0" if torch.cuda.is_available() and args.gpu else "cpu")
+
+    def setup(self,weight):
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.policy.configure(self.config)
         self.policy.set_device(self.device)
+        self.policy.get_model().load_state_dict(torch.load(weight))
 
     
     def preprocess(self,state)->JointState:
-        """transfern state to joint state
+        vx = state[3]*math.cos(math.radians(state[5]))
+        vy = state[3]*math.sin(math.radians(state[5]))
+        v_pref = 1
+        rs = FullState(state[0],state[1],vx,vy,state[2],state[6],state[7],v_pref,math.radians(state[5]))
 
-        Args:
-            state (list): frames state
-        """
-
-        raise NotImplementedError
+        humans_states = state[8:]
+        hs = []
+        for i in range(int(len(humans_states)//7)):
+            index = i*7
+            hs.append(ObservableState(humans_states[index+1],
+                                      humans_states[index+2],
+                                      humans_states[index+3],
+                                      humans_states[index+4],
+                                      humans_states[index+5]))
+        joint_state = JointState(rs,hs)
+        return joint_state
 
 
     def predict(self,state):
--- nav_sim_demo.py
+++ nav_sim_demo.py
@@ -4,14 +4,18 @@ from nav_sim.envs import NavEnvV1
 import json
 import tqdm
 from pprint import pprint
+from nav_alg import NavAlg
 
 config_path = r'config/env_config/env_demo.json'
 config = json.load(open(config_path))
 env = NavEnvV1(config)
 env.reset()
 
+cadrl = NavAlg(".")
+
 for i in tqdm.trange(10000):
-    s = env.step(0)
+    s = env.step(env.action_space[1])
+    print(cadrl.preprocess(s[0]).__dict__)
     print(s)
     env.render()
 
